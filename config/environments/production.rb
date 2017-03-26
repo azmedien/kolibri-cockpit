42dca3be2872aa296e0c9f32053fbf5b4a277d3b
Asen Lekov <asenlekoff@gmail.com>
2017-03-26 20:18:39 +0300

    Updated production configurated

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,8 +36,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.url = 'wss://kolibri.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = [ 'http://kolibri.herokuapp.com', /http:\/\/kolibri.herokuapp.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
@@ -56,6 +56,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "kolibri-cockpit_#{Rails.env}"
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'http://kolibri.herokuapp.com' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
