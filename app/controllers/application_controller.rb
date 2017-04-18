fa55a0f1ff5442d6e91dc740820aad671891e338
Asen Lekov <asenlekoff@gmail.com>
2017-04-18 20:00:10 +0300

    Rescue from 404 and 500

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   add_flash_types :success, :warning, :danger, :info
+
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+  rescue_from ActionController::RoutingError, with: :not_found
+
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_apps
 
@@ -8,6 +12,22 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+  end
+
+  def not_found
+    respond_to do |format|
+      format.html { render file: "#{Rails.root}/public/404", layout: false, status: :not_found }
+      format.xml { head :not_found }
+      format.any { head :not_found }
+    end
+  end
+
+  def error
+    respond_to do |format|
+      format.html { render file: "#{Rails.root}/public/500", layout: false, status: :error }
+      format.xml { head :not_found }
+      format.any { head :not_found }
+    end
   end
 
   def set_apps
