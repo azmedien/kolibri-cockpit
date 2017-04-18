667aa8e64aa4dd66b35eb9f10b320ef6842cae11
Asen Lekov <asenlekoff@gmail.com>
2017-04-18 15:27:20 +0300

    Truncate long URLs and make AWS public

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -44,7 +44,7 @@ CarrierWave.configure do |config|
       :region                 => ENV['S3_REGION']                   # optional, defaults to 'us-east-1'
     }
     config.fog_directory  = ENV['S3_BUCKET_NAME']                   # required
-    config.fog_public     = false                                   # optional, defaults to true
+    config.fog_public     = true                                    # optional, defaults to true
     config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
   end
 end
