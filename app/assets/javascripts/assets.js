c08a3cc3717047c57f8806ad0a3129e64d368547
Asen Lekov <asenlekoff@gmail.com>
2017-06-13 11:21:50 +0300

    UI improvements
    Preaload of assets thumbs.
    Application and assets click improvements.
    Removed netmetrix from build.
    Removed jenkins job from build.
    Updated literals for bundle, versions in prepare.
    Updated literals for name and id in settings.

--- app/assets/javascripts/assets.js
+++ app/assets/javascripts/assets.js
@@ -1,1 +1,11 @@
-// 
+//
+$(document).on("turbolinks:load", function() {
+  $.each($('.preload'), function(index, obj){
+    var img = new Image();
+    img.src = $(obj).attr('data-source');
+
+    $(img).ready(function(){
+      $(obj).attr('src', img.src);
+    })
+  })
+});
--- app/helpers/assets_helper.rb
+++ app/helpers/assets_helper.rb
@@ -5,7 +5,7 @@ module AssetsHelper
   def assets_content_type_icon(asset, size)
     case
     when asset.content_type.start_with?('image')
-      image_tag(asset.file.url, size: size, :class => "img-responsive")
+      image_tag('placeholder.png', size: size, :class => 'img-responsive preload', :data => { :source => asset.file.url })
     when asset.content_type.end_with?('json')
       '<i class="fa fa-file-code-o" style="font-size:32px"></i>'
     else
