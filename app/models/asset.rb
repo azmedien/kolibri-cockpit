9516a8de9c1e6ae95bf7fa9ff2af62392405b9ad
Asen Lekov <asenlekoff@gmail.com>
2017-06-15 12:00:07 +0300

    Fixed where slugs on upload not worked.

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -10,14 +10,21 @@ class Asset < ApplicationRecord
   validates_integrity_of :file
   validates_processing_of :file
 
+  friendly_id :get_filename
+
   def duplicate_file(original)
     copy_carrierwave_file(original, self, :file)
-    
+
     self.slug = original.slug
     self.save!
   end
 
   def internal_id(app_id = self.app_id)
     @internal_id = @internal_id || App.find(app_id).internal_id
+  end
+
+  private
+  def get_filename
+    self.file.filename
   end
 end
