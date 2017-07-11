918c2b673755d04e6c45d6fe00040338a861f2cf
Asen Lekov <asenlekoff@gmail.com>
2017-07-11 17:10:39 +0300

    Permit version code and version name for ios configuration.

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -216,7 +216,7 @@ class AppsController < ApplicationController
         :ios_icon,
         :splash,
         :android_config => [:repository_url, :netmetrix_ua, :netmetrix_url, :bundle_id, :version_code, :version_name],
-        :ios_config => [:repository_url, :netmetrix_ua, :netmetrix_url, :bundle_id])
+        :ios_config => [:repository_url, :netmetrix_ua, :netmetrix_url, :bundle_id, :version_code, :version_name])
     end
 
     def notification_params
