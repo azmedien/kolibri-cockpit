1de7467fba717914c854594b2871d904ea81dda2
Asen Lekov <asenlekoff@gmail.com>
2017-07-12 11:16:16 +0300

    Git restructure

--- app/helpers/git_helper.rb
+++ app/helpers/git_helper.rb
@@ -8,13 +8,14 @@ module GitHelper
 
   def clone_repo(url)
     folder = url.split('/').last
-    Git.clone(url, folder, :path => Rails.root.join('tmp'), :log => Logger.new(STDOUT)) if !repo_exist?(folder)
+    Git.clone(url, folder, path: Rails.root.join('tmp'), log: Logger.new(STDOUT)) unless repo_exist?(folder)
   end
 
   def open_repo(url)
     folder = url.split('/').last
-    clone_repo(url) if !repo_exist?(folder)
-    git = Git.open(Rails.root.join('tmp', folder), :log => Logger.new(STDOUT))
+    clone_repo(url) unless repo_exist?(folder)
+    git = Git.open(Rails.root.join('tmp', folder), log: Logger.new(STDOUT))
+    git.checkout('master')
     git.reset_hard()
     git.pull()
 
@@ -22,7 +23,6 @@ module GitHelper
   end
 
   def manipulate_repo(url, app, user)
-
     branch_name = app.internal_name.parameterize
     origin_app = App.friendly.find(app.android_config['origin']) if app.android_config['origin']
 
@@ -31,8 +31,6 @@ module GitHelper
 
     repo.config('user.name', "#{user.first_name} #{user.last_name}")
     repo.config('user.email', user.email)
-
-    repo.checkout('master')
 
     # Trying to find origin app branch we want to branch from. We want to skip this if we alredy cloned the app
     if origin_app && (repo_branches[branch_name].nil? || repo_branches["origin/#{branch_name}"].nil?)
@@ -45,20 +43,23 @@ module GitHelper
     repo.branch(branch_name).checkout
 
     begin
-      repo.pull('origin', repo.branch(branch_name))
-    rescue
-    ensure
-      begin
-        repo.chdir do
-          yield repo
-          repo.add(:all=>true)
-          repo.commit('Project configured by Kolibri Cockpit')
-          repo.push('origin', app.internal_name.parameterize)
-        end
-       ensure
-         repo.checkout('master')
-         repo.branch(app.internal_name.parameterize).delete
-       end
+        repo.pull('origin', repo.branch(branch_name))
+      rescue
+      ensure
+        begin
+           repo.chdir do
+             yield repo
+             repo.add(all: true)
+             repo.commit('Project configured by Kolibri Cockpit')
+             repo.push('origin', app.internal_name.parameterize)
+           end
+         ensure
+           repo.checkout('master')
+           repo.branch(app.internal_name.parameterize).delete
+
+           folder = url.split('/').last
+           FileUtils.rm_rf(Rails.root.join('tmp', folder))
+         end
       end
     end
 end
