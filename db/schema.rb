43bc3d45383f311f5bbbb6486ca41257f8b682fd
Asen Lekov <asenlekoff@gmail.com>
2017-07-17 23:04:50 +0300

    Improve UX/UI.
    Modal dialogs when creating apps or assets.

--- db/schema.rb
+++ db/schema.rb
@@ -16,181 +16,181 @@ ActiveRecord::Schema.define(version: 20170710205805) do
   enable_extension "plpgsql"
   enable_extension "hstore"
 
-  create_table "apps", force: :cascade do |t|
-    t.string   "internal_name"
-    t.string   "internal_id"
-    t.json     "runtime",          default: "{}", null: false
-    t.hstore   "android_config",   default: {},   null: false
-    t.hstore   "ios_config",       default: {},   null: false
-    t.integer  "user_id"
-    t.datetime "created_at",                      null: false
-    t.datetime "updated_at",                      null: false
-    t.string   "android_icon"
-    t.string   "ios_icon"
-    t.string   "splash"
-    t.string   "slug"
-    t.string   "android_firebase"
-    t.string   "ios_firebase"
+  create_table "apps", id: :serial, force: :cascade do |t|
+    t.string "internal_name"
+    t.string "internal_id"
+    t.json "runtime", default: "{}", null: false
+    t.hstore "android_config", default: {}, null: false
+    t.hstore "ios_config", default: {}, null: false
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "android_icon"
+    t.string "ios_icon"
+    t.string "splash"
+    t.string "slug"
+    t.string "android_firebase"
+    t.string "ios_firebase"
     t.index ["android_config"], name: "index_apps_on_android_config", using: :gin
-    t.index ["internal_id"], name: "index_apps_on_internal_id", using: :btree
-    t.index ["internal_name"], name: "index_apps_on_internal_name", using: :btree
+    t.index ["internal_id"], name: "index_apps_on_internal_id"
+    t.index ["internal_name"], name: "index_apps_on_internal_name"
     t.index ["ios_config"], name: "index_apps_on_ios_config", using: :gin
-    t.index ["user_id"], name: "index_apps_on_user_id", using: :btree
+    t.index ["user_id"], name: "index_apps_on_user_id"
   end
 
-  create_table "assets", force: :cascade do |t|
-    t.string   "file"
-    t.integer  "app_id"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
-    t.string   "content_type"
-    t.integer  "file_size"
-    t.string   "slug"
-    t.index ["app_id"], name: "index_assets_on_app_id", using: :btree
+  create_table "assets", id: :serial, force: :cascade do |t|
+    t.string "file"
+    t.integer "app_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "content_type"
+    t.integer "file_size"
+    t.string "slug"
+    t.index ["app_id"], name: "index_assets_on_app_id"
   end
 
-  create_table "builds", force: :cascade do |t|
-    t.string   "build_id"
-    t.integer  "app_id"
-    t.datetime "created_at",                     null: false
-    t.datetime "updated_at",                     null: false
-    t.string   "url"
-    t.string   "platform",   default: "unknown", null: false
-    t.integer  "user_id"
-    t.string   "stage"
-    t.integer  "code",       default: 0,         null: false
-    t.string   "message"
-    t.index ["app_id"], name: "index_builds_on_app_id", using: :btree
-    t.index ["build_id"], name: "index_builds_on_build_id", using: :btree
-    t.index ["platform", "build_id"], name: "index_builds_on_platform_and_build_id", unique: true, using: :btree
-    t.index ["user_id"], name: "index_builds_on_user_id", using: :btree
+  create_table "builds", id: :serial, force: :cascade do |t|
+    t.string "build_id"
+    t.integer "app_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "url"
+    t.string "platform", default: "unknown", null: false
+    t.integer "user_id"
+    t.string "stage"
+    t.integer "code", default: 0, null: false
+    t.string "message"
+    t.index ["app_id"], name: "index_builds_on_app_id"
+    t.index ["build_id"], name: "index_builds_on_build_id"
+    t.index ["platform", "build_id"], name: "index_builds_on_platform_and_build_id", unique: true
+    t.index ["user_id"], name: "index_builds_on_user_id"
   end
 
-  create_table "friendly_id_slugs", force: :cascade do |t|
-    t.string   "slug",                      null: false
-    t.integer  "sluggable_id",              null: false
-    t.string   "sluggable_type", limit: 50
-    t.string   "scope"
+  create_table "friendly_id_slugs", id: :serial, force: :cascade do |t|
+    t.string "slug", null: false
+    t.integer "sluggable_id", null: false
+    t.string "sluggable_type", limit: 50
+    t.string "scope"
     t.datetime "created_at"
-    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true, using: :btree
-    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type", using: :btree
-    t.index ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id", using: :btree
-    t.index ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type", using: :btree
+    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
+    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
+    t.index ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id"
+    t.index ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type"
   end
 
-  create_table "roles", force: :cascade do |t|
-    t.string   "name"
-    t.string   "resource_type"
-    t.integer  "resource_id"
-    t.decimal  "abilities",     default: "0.0", null: false
+  create_table "roles", id: :serial, force: :cascade do |t|
+    t.string "name"
+    t.string "resource_type"
+    t.integer "resource_id"
+    t.decimal "abilities", default: "0.0", null: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.index ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id", using: :btree
-    t.index ["name"], name: "index_roles_on_name", using: :btree
+    t.index ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id"
+    t.index ["name"], name: "index_roles_on_name"
   end
 
-  create_table "rpush_apps", force: :cascade do |t|
-    t.string   "name",                                null: false
-    t.string   "environment"
-    t.text     "certificate"
-    t.string   "password"
-    t.integer  "connections",             default: 1, null: false
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.string   "type",                                null: false
-    t.string   "auth_key"
-    t.string   "client_id"
-    t.string   "client_secret"
-    t.string   "access_token"
+  create_table "rpush_apps", id: :serial, force: :cascade do |t|
+    t.string "name", null: false
+    t.string "environment"
+    t.text "certificate"
+    t.string "password"
+    t.integer "connections", default: 1, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "type", null: false
+    t.string "auth_key"
+    t.string "client_id"
+    t.string "client_secret"
+    t.string "access_token"
     t.datetime "access_token_expiration"
   end
 
-  create_table "rpush_feedback", force: :cascade do |t|
-    t.string   "device_token", limit: 64, null: false
-    t.datetime "failed_at",               null: false
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
-    t.integer  "app_id"
-    t.index ["device_token"], name: "index_rpush_feedback_on_device_token", using: :btree
+  create_table "rpush_feedback", id: :serial, force: :cascade do |t|
+    t.string "device_token", limit: 64, null: false
+    t.datetime "failed_at", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "app_id"
+    t.index ["device_token"], name: "index_rpush_feedback_on_device_token"
   end
 
-  create_table "rpush_notifications", force: :cascade do |t|
-    t.integer  "badge"
-    t.string   "device_token",      limit: 64
-    t.string   "sound",                        default: "default"
-    t.text     "alert"
-    t.text     "data"
-    t.integer  "expiry",                       default: 86400
-    t.boolean  "delivered",                    default: false,     null: false
+  create_table "rpush_notifications", id: :serial, force: :cascade do |t|
+    t.integer "badge"
+    t.string "device_token", limit: 64
+    t.string "sound", default: "default"
+    t.text "alert"
+    t.text "data"
+    t.integer "expiry", default: 86400
+    t.boolean "delivered", default: false, null: false
     t.datetime "delivered_at"
-    t.boolean  "failed",                       default: false,     null: false
+    t.boolean "failed", default: false, null: false
     t.datetime "failed_at"
-    t.integer  "error_code"
-    t.text     "error_description"
+    t.integer "error_code"
+    t.text "error_description"
     t.datetime "deliver_after"
-    t.datetime "created_at",                                       null: false
-    t.datetime "updated_at",                                       null: false
-    t.boolean  "alert_is_json",                default: false
-    t.string   "type",                                             null: false
-    t.string   "collapse_key"
-    t.boolean  "delay_while_idle",             default: false,     null: false
-    t.text     "registration_ids"
-    t.integer  "app_id",                                           null: false
-    t.integer  "retries",                      default: 0
-    t.string   "uri"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.boolean "alert_is_json", default: false
+    t.string "type", null: false
+    t.string "collapse_key"
+    t.boolean "delay_while_idle", default: false, null: false
+    t.text "registration_ids"
+    t.integer "app_id", null: false
+    t.integer "retries", default: 0
+    t.string "uri"
     t.datetime "fail_after"
-    t.boolean  "processing",                   default: false,     null: false
-    t.integer  "priority"
-    t.text     "url_args"
-    t.string   "category"
-    t.boolean  "content_available",            default: false
-    t.text     "notification"
-    t.index ["delivered", "failed"], name: "index_rpush_notifications_multi", where: "((NOT delivered) AND (NOT failed))", using: :btree
+    t.boolean "processing", default: false, null: false
+    t.integer "priority"
+    t.text "url_args"
+    t.string "category"
+    t.boolean "content_available", default: false
+    t.text "notification"
+    t.index ["delivered", "failed"], name: "index_rpush_notifications_multi", where: "((NOT delivered) AND (NOT failed))"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "first_name",             default: "", null: false
-    t.string   "last_name",              default: "", null: false
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+  create_table "users", id: :serial, force: :cascade do |t|
+    t.string "first_name", default: "", null: false
+    t.string "last_name", default: "", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.string   "confirmation_token"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
-    t.string   "unconfirmed_email"
-    t.integer  "failed_attempts",        default: 0,  null: false
-    t.string   "unlock_token"
+    t.string "unconfirmed_email"
+    t.integer "failed_attempts", default: 0, null: false
+    t.string "unlock_token"
     t.datetime "locked_at"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.string   "invitation_token"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "invitation_token"
     t.datetime "invitation_created_at"
     t.datetime "invitation_sent_at"
     t.datetime "invitation_accepted_at"
-    t.integer  "invitation_limit"
-    t.string   "invited_by_type"
-    t.integer  "invited_by_id"
-    t.integer  "invitations_count",      default: 0
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
-    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
-    t.index ["invitation_token"], name: "index_users_on_invitation_token", unique: true, using: :btree
-    t.index ["invitations_count"], name: "index_users_on_invitations_count", using: :btree
-    t.index ["invited_by_id"], name: "index_users_on_invited_by_id", using: :btree
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-    t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true, using: :btree
+    t.integer "invitation_limit"
+    t.string "invited_by_type"
+    t.integer "invited_by_id"
+    t.integer "invitations_count", default: 0
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["invitation_token"], name: "index_users_on_invitation_token", unique: true
+    t.index ["invitations_count"], name: "index_users_on_invitations_count"
+    t.index ["invited_by_id"], name: "index_users_on_invited_by_id"
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
 
   create_table "users_roles", id: false, force: :cascade do |t|
     t.integer "user_id"
     t.integer "role_id"
-    t.index ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id", using: :btree
+    t.index ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id"
   end
 
 end
