c9e17b55ee180e047b0e857e9c452e65c1ad08aa
Asen Lekov <asenlekoff@gmail.com>
2017-07-17 16:48:53 +0300

    Refactor application configuration job.
    Suppress SSH logs.
    Log all configure procedure.
    Introduce Android or iOS configure services.
    Added FileHelper.

---
+++ lib/file_helper.rb
@@ -0,0 +1,11 @@
+module FileHelper
+  def modify_plist(file)
+    sanitarized_plist = IO.read(file).force_encoding("us-ascii").encode("utf-8", replace: '')
+
+    plist = Plist.parse_xml(sanitarized_plist)
+    yield plist
+  ensure
+    plist_string = Plist::Emit.dump(plist)
+    File.write(file, plist_string)
+  end
+end
