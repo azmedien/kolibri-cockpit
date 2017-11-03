1f907c62d65de7f582dda692598b776a10ad5dcc
Asen Lekov <asenlekoff@gmail.com>
2017-11-03 17:37:09 +0200

    Try fixing Encoding::InvalidByteSequenceError (“\xC3” on US-ASCII) when modifying plist for iOS apps

--- lib/file_helper.rb
+++ lib/file_helper.rb
@@ -1,6 +1,6 @@
 module FileHelper
   def modify_plist(file)
-    sanitarized_plist = IO.read(file).force_encoding("us-ascii").encode("utf-8", replace: '')
+    sanitarized_plist = IO.read(file).force_encoding("ISO-8859-1").encode("UTF-8")
 
     plist = Plist.parse_xml(sanitarized_plist)
     yield plist
