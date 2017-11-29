57fd1d3cab97d3cb836165e3893a74b946282d68
Asen Lekov <asenlekoff@gmail.com>
2017-11-29 23:09:09 +0200

    Publishing improvements.
    Added tags to the git repository on publish.
    Added publishing profile selection for iOS.
    Updated publishing profiles.

--- app/jobs/configure_app_job.rb
+++ app/jobs/configure_app_job.rb
@@ -39,8 +39,6 @@ class ConfigureAppJob < ApplicationJob
       manipulate_repo repo, app, user, tag_name do |_git|
         service = AppConfigureService.new('android', '.', app)
         service.configure_it
-        _git.add_tag("#{app.internal_name.parameterize}-#{app.android_config[version_name]}(#{app.android_config[version_code]})")
-        _git.push
       end
     end
   end
@@ -59,8 +57,6 @@ class ConfigureAppJob < ApplicationJob
     manipulate_repo repo, app, user, tag_name do |_git|
       service = AppConfigureService.new('ios', '.', app)
       service.configure_it
-      _git.add_tag("#{app.internal_name.parameterize}-#{app.ios_config[version_name]}(#{app.ios_config[version_code]})")
-      _git.push
     end
   end
 
